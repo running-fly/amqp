7f115b98ab4f7ab9eb6ffe643793117a93fb2df4
xueqing123 <1255305854@qq.com>
2018-12-10 14:32:39 +0800

    多消费者

--- emit_log_topic.php
+++ emit_log_topic.php
@@ -2,9 +2,10 @@
 require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
+//php emit_log_topic.php  a.a
 $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
 $channel = $connection->channel();
-$channel->exchange_declare('topic_logs', 'topic', false, false, false);
+$channel->exchange_declare('topic_logs', 'topic', false, true, false);
 $routing_key = isset($argv[1]) && !empty($argv[1]) ? $argv[1] : 'anonymous.info';
 $data = implode(' ', array_slice($argv, 2));
 if (empty($data)) {
